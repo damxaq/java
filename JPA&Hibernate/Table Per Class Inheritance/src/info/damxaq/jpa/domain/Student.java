bd661eed355595b7b8dba1cf2869f25fa2b7a5f0
Damian Celico <damiancelico@gmail.com>
2017-08-19 20:28:42 +0200

    Using inheritance in tables

---
+++ JPA&Hibernate/Table Per Class Inheritance/src/info/damxaq/jpa/domain/Student.java
@@ -0,0 +1,20 @@
+package info.damxaq.jpa.domain;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Student extends Person{
+
+	private double averageGrade;
+
+	public double getAverageGrade() {
+		return averageGrade;
+	}
+
+	public void setAverageGrade(double averageGrade) {
+		this.averageGrade = averageGrade;
+	}
+	
+	
+	
+}
